6eaf6b5a8c779f10af9fc37dcff7f411e1f8a14a
many <emmanuel@archiemd.com>
2022-08-17 17:29:20 -0400

    new scene

--- public/js/example.js
+++ public/js/example.js
@@ -1,3 +1,7 @@
+
+// import * as THREE from 'three';
+// import { GLTFLoader } from './libs/threejs/GLTFLoader.js';
+
 var width, height;
 var viewAngle = 45,
 	near = 1,
@@ -28,24 +32,37 @@ function startScene(container) {
 
 	scene = new THREE.Scene();
 
-	// Load models
-	var loader = new THREE.ObjectLoader();
+	const loader = new THREE.GLTFLoader().setPath( 'model/' );
+	loader.load( 'scene.gltf', function ( gltf ) {
 
-	loader.load("model/model.json", function (object) {
-		sceneObject = object;
-		// console.log(sceneObject);
-		sceneObject.scale.set(13, 13, 13);
-		sceneObject.position.set(0, 0, 0);
-		scene.add(sceneObject);
-
-		var axes = new THREE.AxisHelper(700);
-		scene.add(axes);
-
+		scene.add( gltf.scene );
+		camera = gltf.cameras[0];
 		addLights();
+		onWindowResize();
 		addControls();
-
 		animate();
-	});
+
+	} );
+
+	// Load models
+	// var loader = new THREE.SceneLoader();
+
+	// loader.load("model/newscene.json", function (e) {
+	// 	scene = e.scene;
+	// 	// sceneObject = object;
+	// 	// console.log(sceneObject);
+	// 	sceneObject.scale.set(13, 13, 13);
+	// 	sceneObject.position.set(0, 0, 0);
+	// 	scene.add(sceneObject);
+
+	// 	var axes = new THREE.AxisHelper(700);
+	// 	scene.add(axes);
+
+	// 	addLights();
+	// 	// addControls();
+
+	// 	animate();
+	// });
 
 	function addLights() {
 		// Lights
@@ -69,15 +86,19 @@ function startScene(container) {
 		camera = new THREE.PerspectiveCamera(viewAngle, aspect, near, far);
 		// Controls
 		var options = {
-			speedFactor: 0.5,
+			speedFactor: 0.04,
 			delta: 1,
 			rotationFactor: 0.002,
 			maxPitch: 55,
 			hitTest: true,
 			hitTestDistance: 40
 		};
+		// var pos = camera.position;
+		
+		// camera.position.set(0,0,0);
+		// camera.quaternion.identity();
 		controls = new TouchControls(container.parent(), camera, options);
-		controls.setPosition(0, 35, 400);
+		// controls.setPosition(pos);
 		controls.addToScene(scene);
 		// controls.setRotation(0.15, -0.15);
 	}
@@ -100,30 +121,32 @@ function animate() {
 
 	requestAnimationFrame(animate);
 
-	controls.update();
+	if(controls) {
+		controls.update();
 
-	// Mouse hit-testing:
-	var vector = new THREE.Vector3(controls.mouse.x, controls.mouse.y, 1);
-	vector.unproject(camera);
-
-	var raycaster = new THREE.Raycaster(controls.fpsBody.position, vector.sub(controls.fpsBody.position).normalize());
-
-	var intersects = raycaster.intersectObjects(sceneObject.children);
-	if (intersects.length > 0) {
-		if (intersected != intersects[0].object) {
+		// Mouse hit-testing:
+		var vector = new THREE.Vector3(controls.mouse.x, controls.mouse.y, 1);
+		vector.unproject(camera);
+	
+		var raycaster = new THREE.Raycaster(controls.fpsBody.position, vector.sub(controls.fpsBody.position).normalize());
+	
+		var intersects = raycaster.intersectObjects(scene.children);
+		if (intersects.length > 0) {
+			if (intersected != intersects[0].object) {
+				if (intersected) intersected.material.emissive.setHex(intersected.currentHex);
+				intersected = intersects[0].object;
+				// console.log(intersects);
+				intersected.currentHex = intersected.material.emissive.getHex();
+				intersected.material.emissive.setHex(0xdd0090);
+			}
+		} else {
 			if (intersected) intersected.material.emissive.setHex(intersected.currentHex);
-			intersected = intersects[0].object;
-			// console.log(intersects);
-			intersected.currentHex = intersected.material.emissive.getHex();
-			intersected.material.emissive.setHex(0xdd0090);
+			intersected = null;
 		}
-	} else {
-		if (intersected) intersected.material.emissive.setHex(intersected.currentHex);
-		intersected = null;
 	}
 
 	renderer.render(scene, camera);
-	window.scrollTo(0, 200);
+	// window.scrollTo(0, 200);
 }
 
 function onWindowResize() {
@@ -166,7 +189,7 @@ function toggleFullScreen() {
 	  cancelFullScreen.call(doc);
 	}
   }
-  
+
   var goFS = document.getElementById('goFS');
   goFS.addEventListener(
     'click',
